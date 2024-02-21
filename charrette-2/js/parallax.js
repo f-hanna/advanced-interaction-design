1106fc4d6febc9100d7d5319131ebe03f7500cd6
f-hanna <77943310+f-hanna@users.noreply.github.com>
2024-02-21 09:02:20 -0600

    22

--- charrette-2/js/parallax.js
+++
@@ -1,9 +0,0 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-const smoother = ScrollSmoother.create({
- content: "#content",
- smooth: 3,
- effects: true
-});
-
-smoother.effects("img", { speed: "auto" });
