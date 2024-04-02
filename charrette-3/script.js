347a2c31c61d7e5410b97b1507e8029b1411e3d2
f-hanna <77943310+f-hanna@users.noreply.github.com>
2024-04-02 01:21:11 -0500

    site 2 + css done

--- charrette-3/script.js
+++ charrette-3/script.js
@@ -5,3 +5,20 @@ function toggleBlur() {
         lenses[i].classList.toggle('blur-effect');
     }
 }
+
+//SITE 2
+let grayscale = 100
+let round = 5
+
+function decreaseGrayscale() {
+    round = round - 1
+    var img = document.getElementById('img-rainbow');
+    var rainbowBtn = document.getElementById('rainbow-btn');
+    grayscale = grayscale - 20;
+    rainbowBtn.innerHTML = "Load " + round + " more times"
+    if (round < 1) {
+        rainbowBtn.innerHTML = "Done Loading"
+        grayscale = 0; // Ensure grayscale value doesn't go below 0
+    }
+    img.style.filter = `grayscale(${grayscale}%)`; // Apply new grayscale value
+}
