a26acd1ad5280ec2bccdcbafb29d67f6d6f1c203
f-hanna <77943310+f-hanna@users.noreply.github.com>
2024-04-01 21:09:48 -0500

    site 2

--- charrette-3/script.js
+++ charrette-3/script.js
@@ -1,6 +1,5 @@
+// SITE 1
 function toggleBlur() {
-    console.log("clicked!")
-    
     var lenses = document.querySelectorAll('.lens-background');
     for (var i = 0; i < lenses.length; i++) {
         lenses[i].classList.toggle('blur-effect');
