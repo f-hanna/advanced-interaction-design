4f748e558be8ec397b967e9c138f5c2eb321402c
f-hanna <77943310+f-hanna@users.noreply.github.com>
2024-04-03 02:52:26 -0500

    done with 10 sites

--- charrette-3/script.js
+++ charrette-3/script.js
@@ -107,7 +107,7 @@ function solve() {
 function zoom() {
     var img = document.getElementById('img-planet');
     img.style.opacity = "100%";
-    img.style.transform = 'scale(10)'; 
+    img.style.transform = 'scale(10)';
 }
 
 // SITE 8
@@ -117,10 +117,71 @@ function google() {
     var img = document.getElementById("img-holder")
     img.style.display = "block"
 
-    setTimeout(function() {
+    setTimeout(function () {
         // document.getElementById("image-wrapper").style.height = "40vh"
         img.src = "https://media.licdn.com/dms/image/C5112AQESSXLEM7P8yw/article-inline_image-shrink_400_744/0/1552144216522?e=1716422400&v=beta&t=n6pWs_TvfstHfA0P_OQePB4JccxGaHOtisQexZSXFuU"
     }, 5000); // 5000 milliseconds = 5 seconds
+
+}
+
+// SITE 9
+
+document.addEventListener("DOMContentLoaded", function() {
+    shuffle()
+});
+
+function swapTiles(cell1, cell2) {
+    var temp = document.getElementById(cell1).className;
+    document.getElementById(cell1).className = document.getElementById(cell2).className;
+    document.getElementById(cell2).className = temp;
+}
+
+function shuffle() {
+    //Use nested loops to access each cell of the 3x3 grid
+    for (var row = 1; row <= 3; row++) { //For each row of the 3x3 grid
+        for (var column = 1; column <= 3; column++) { //For each column in this row
+
+            var row2 = Math.floor(Math.random() * 3 + 1); //Pick a random row from 1 to 3
+            var column2 = Math.floor(Math.random() * 3 + 1); //Pick a random column from 1 to 3
+
+            swapTiles("cell" + row + column, "cell" + row2 + column2); //Swap the look & feel of both cells
+        }
+    }
+}
+
+function clickTile(row, column) {
+    var cell = document.getElementById("cell" + row + column);
+    var tile = cell.className;
+    if (tile != "tile9") {
+        //Checking if white tile on the right
+        if (column < 3) {
+            if (document.getElementById("cell" + row + (column + 1)).className == "tile9") {
+                swapTiles("cell" + row + column, "cell" + row + (column + 1));
+                return;
+            }
+        }
+        //Checking if white tile on the left
+        if (column > 1) {
+            if (document.getElementById("cell" + row + (column - 1)).className == "tile9") {
+                swapTiles("cell" + row + column, "cell" + row + (column - 1));
+                return;
+            }
+        }
+        //Checking if white tile is above
+        if (row > 1) {
+            if (document.getElementById("cell" + (row - 1) + column).className == "tile9") {
+                swapTiles("cell" + row + column, "cell" + (row - 1) + column);
+                return;
+            }
+        }
+        //Checking if white tile is below
+        if (row < 3) {
+            if (document.getElementById("cell" + (row + 1) + column).className == "tile9") {
+                swapTiles("cell" + row + column, "cell" + (row + 1) + column);
+                return;
+            }
+        }
+    }
 
 }
 
@@ -137,7 +198,7 @@ function countdown() {
     var countdownElement = document.getElementById("countdown");
 
     // Update the countdown every second
-    var countdownInterval = setInterval(function() {
+    var countdownInterval = setInterval(function () {
         countdownElement.textContent = countdownDuration;
         countdownDuration--;
 
