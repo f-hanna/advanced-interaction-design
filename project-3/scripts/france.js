e892003a831cc57fa607483dcb7f906885858ac7
f-hanna <77943310+f-hanna@users.noreply.github.com>
2024-04-19 16:35:36 -0500

    gjdklfgh

--- project-3/scripts/france.js
+++ project-3/scripts/france.js
@@ -1,11 +1,25 @@
 // GLOBAL VARIABLES
 let currentQuestion = "q1"
 
+// FRA OBJECT
+let questionObj = {
+    "q1": {
+        "question": "Testing Question 1 (replace)",
+        "movement": "Rococo",
+        "questionOptions":  {
+            "a": "option 1 (replace)",
+            "b": "option 2 (replace)",
+            "c": "option 3 (replace)",
+            "d": "option 4 (replace)"
+        },
+        "answer": "d"
+    }
+}
+
 function JSON() {
     fetch('https://f-hanna.github.io/advanced-interaction-design/project-3/json/fra-q.json')
         .then((response) => response.json())
         .then((json) => console.log(json));
-
     // return json
 }
 
