a7d942b5425ad52d0c4fe6b48cca9bb2d54e731c
f-hanna <77943310+f-hanna@users.noreply.github.com>
2024-04-22 16:19:01 -0500

    start to style + add more content
    dleted json, go to earlier commit to check
    
    functionality + css
    
    think about pamplet

--- project-3/scripts/france.js
+++ project-3/scripts/france.js
@@ -1,26 +1,15 @@
-// GLOBAL VARIABLES
-let currentQuestion = "q1"
+let paintings = [
+    "rococo",
+    "neo"
+]
 
-// FRA OBJECT
-let questionObj = {
-    "q1": {
-        "question": "Testing Question 1 (replace)",
-        "movement": "Rococo",
-        "questionOptions":  {
-            "a": "option 1 (replace)",
-            "b": "option 2 (replace)",
-            "c": "option 3 (replace)",
-            "d": "option 4 (replace)"
-        },
-        "answer": "d"
-    }
-}
+let userAnswer = [paintings.length]
 
-function JSON() {
-    fetch('https://f-hanna.github.io/advanced-interaction-design/project-3/json/fra-q.json')
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-    // return json
+function close() {
+    console.log("close")
+    $('#fra-quiz-container').hide();
+    $(".overlay").hide()
+    
 }
 
 $("#btn-france-quiz").click(function () {
@@ -28,10 +17,52 @@ $("#btn-france-quiz").click(function () {
     $('#fra-quiz-container').css("display", "flex");
     if ($('#fra-quiz-container').is(":visible")) {
         $(".overlay").show()
+        openQuestion("rococo-content")
+    }
+})
+
+function openQuestion(movement) {
+
+    console.log(movement)
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    let currentTab = document.getElementById(movement)
+    currentTab.style.display = "block";
+  }
+
+function checkAnswer() {
+    console.log("hi!")
+
+    for (let i = 0; i < paintings.length; i++) {
+        var movement = paintings[i]
+        userAnswer[i] = $(`input[type='radio'][name='${movement}']:checked`).val();     
+        
+    }
+    console.log(userAnswer);
+
+    for (let i = 0; i < paintings.length; i++) {
+        if (userAnswer[i] == "true") {
+            document.getElementById(paintings[i]).src = "./img/" +  paintings[i] + "-clear.jpg"         
+        }         
+        
     }
 
-    // ADD IN THE OPTIONS
-    JSON()
-    // console.log(questions)
+    $('#fra-quiz-container').hide();
+    $(".overlay").hide()
 
-});
+}
+
