f3652c6ba21b372cb0e233e6125c9ed504cc1a84
f-hanna <77943310+f-hanna@users.noreply.github.com>
2024-04-23 22:27:44 -0500

    clean up

--- project-3/scripts/france.js
+++ project-3/scripts/france.js
@@ -16,68 +16,57 @@ function closeQuiz() {
     
 }
 
-
-function openQuiz() {
-    let pamphlet = document.querySelector(".quiz-container")
-    pamphlet.style.opacity = "1"
-    pamphlet.style.top = "70%"
-    $(".overlay").show()
-
-    document.getElementById("page-trigger").style.display = "none"
-    let blockers = document.querySelectorAll('.blocker');
-    blockers.forEach(blocker => {
-        blocker.style.display = "none"
-    });
-
-    // let pages = document.querySelectorAll()
-
-}
+$("#btn-france-quiz").click(function () {
+    $('#fra-quiz-container').show();
+    $('#fra-quiz-container').css("display", "flex");
+    if ($('#fra-quiz-container').is(":visible")) {
+        $(".overlay").show()
+        openQuestion("rococo-content")
+    }
+})
 
 function openQuestion(movement) {
 
     console.log(movement)
     // Declare all variables
     var i, tabcontent, tablinks;
-
+  
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("content");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+      tabcontent[i].style.display = "none";
     }
-
+  
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
+  
     // Show the current tab, and add an "active" class to the button that opened the tab
     let currentTab = document.getElementById(movement)
     currentTab.style.display = "block";
-}
+  }
 
 function checkAnswer() {
     console.log("hi!")
 
     for (let i = 0; i < paintings.length; i++) {
         var movement = paintings[i]
-        userAnswer[i] = $(`input[type='radio'][name='${movement}']:checked`).val();
-
+        userAnswer[i] = $(`input[type='radio'][name='${movement}']:checked`).val();     
+        
     }
     console.log(userAnswer);
 
     for (let i = 0; i < paintings.length; i++) {
         if (userAnswer[i] == "true") {
-            document.getElementById(paintings[i]).src = "./img/" + paintings[i] + "-clear.jpg"
-        }
-
+            document.getElementById(paintings[i]).src = "./img/" +  paintings[i] + "-clear.jpg"         
+        }         
+        
     }
 
-    closeQuiz()
-
-    // $(".overlay").hide()
+    $('#fra-quiz-container').hide();
+    $(".overlay").hide()
 
 }
-
-close
 
